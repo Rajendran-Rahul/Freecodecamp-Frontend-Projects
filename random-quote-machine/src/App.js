0690d0ada3675ccb52ca3710f138d424fe990139
Rahul Rajendran <rahul.raj@anywhere.co>
2024-09-19 01:46:00 +0530

    quote-generator

---
+++ random-quote-machine/src/App.js
@@ -0,0 +1,12 @@
+import QuoteMachine from "components/quote-machine";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteMachine />
+    </div>
+  );
+}
+
+export default App;
